6569a09d0d08df5f777df8df8a0a0f45cb0a71bc
Lachlan Campbell <lachlan@watershedclimate.com>
2026-09-07 22:58:51 -0700

    Use astro fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: "Acid Grotesk",
+            cssVariable: "--font-display",
+            options: {
+                variants: [
+                    {
+                        weight: 500,
+                        style: "normal",
+                        display: "swap",
+                        src: ["./src/assets/fonts/AcidGrotesk-Medium.woff2"],
+                    },
+                ],
+            },
+        },
+    ],
+});
