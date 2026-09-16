75d0d1941f4fee85df28ff3546ab88b6317d6671
Matthew Stanciu <mattbstanciu@gmail.com>
2026-09-15 21:41:38 -0700

    Revert "test"
    This reverts commit d649791b87620aadb0612aa980392c9c15340aad.

--- src/lib/datetime.ts
+++ src/lib/datetime.ts
@@ -37,7 +37,7 @@ function wallTimeAsUtc(date: number, timeZone: string): number {
   );
 }
 
-export const SHOW_WALL_TIME = "21:00:00";
+const SHOW_WALL_TIME = "21:00:00";
 const SHOW_TIME_ZONE = "America/Los_Angeles";
 export const SHOW_END_OFFSET_MS = 6 * 3_600_000;
 
